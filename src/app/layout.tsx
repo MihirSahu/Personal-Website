2d7c7a6d577973de536eeeb857f12e4f66607c79
Mihir Sahu <2002mihir@gmail.com>
2025-05-02 23:00:05 -0500

    feat: add posthog analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Link from "next/link";
 import ProfileImage from "./components/ProfileImage";
 import Image from "next/image";
+import { PostHogProvider } from "./providers";
 
 export const metadata: Metadata = {
   title: "Mihir's Blog",
@@ -17,42 +18,54 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="page">
-          <nav>
-            <div className="pfp-div">
-              <ProfileImage />
+        <PostHogProvider>
+          <div className="page">
+            <nav>
+              <div className="pfp-div">
+                <ProfileImage />
+              </div>
+              <div className="links">
+                <Link href="/thoughts">thoughts</Link>
+                <Link href="/work">work</Link>
+                <Link href="/contact">contact</Link>
+              </div>
+            </nav>
+            {children}
+            <div className="placeholder">
+              <Link
+                href="https://x.com/TheMihirSahu"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  height={30}
+                  width={30}
+                  src="/twitter.svg"
+                  alt="Twitter"
+                />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/in/the-mihir-sahu/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  height={30}
+                  width={30}
+                  src="/linkedin.svg"
+                  alt="LinkedIn"
+                />
+              </Link>
+              <Link
+                href="mailto:mihir@mihirsahu.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image height={30} width={30} src="/mail.svg" alt="Email" />
+              </Link>
             </div>
-            <div className="links">
-              <Link href="/thoughts">thoughts</Link>
-              <Link href="/work">work</Link>
-              <Link href="/contact">contact</Link>
-            </div>
-          </nav>
-          {children}
-          <div className="placeholder">
-            <Link
-              href="https://x.com/TheMihirSahu"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image height={30} width={30} src="/twitter.svg" alt="Twitter" />
-            </Link>
-            <Link
-              href="https://www.linkedin.com/in/the-mihir-sahu/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image height={30} width={30} src="/linkedin.svg" alt="LinkedIn" />
-            </Link>
-            <Link
-              href="mailto:mihir@mihirsahu.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image height={30} width={30} src="/mail.svg" alt="Email" />
-            </Link>
           </div>
-        </div>
+        </PostHogProvider>
       </body>
     </html>
   );
