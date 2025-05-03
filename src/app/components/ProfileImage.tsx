7c2b0cb3f2eaf56907ba9d0f5c97022dbcd11015
Mihir Sahu <2002mihir@gmail.com>
2025-05-02 22:34:07 -0500

    feat: use Next.js Image

--- src/app/components/ProfileImage.tsx
+++ src/app/components/ProfileImage.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
@@ -8,7 +9,7 @@ export default function ProfileImage() {
   
   return pathname === '/' ? (
     <Link className="pfp-link" href="/">
-      <img className="pfp" src="/Mihir.jpg" alt="Mihir" />
+      <Image width={100} height={100} className="pfp" src="/Mihir.jpg" alt="Mihir" />
     </Link>
   ) : (
     <Link className="hover-expand" href="/">
