1312fb8ee80f06f85bc89b81dea9050f2258dfa5
Mihir Sahu <2002mihir@gmail.com>
2025-05-05 21:58:41 -0500

    fix: build errors

--- src/app/thoughts/[slug]/page.tsx
+++ src/app/thoughts/[slug]/page.tsx
@@ -12,7 +12,7 @@ export async function generateStaticParams() {
     }))
 }
 
-export default async function Post({ params }: { params: { slug: string } }) {
+export default async function Post({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
   const { default: Post } = await import(`../posts/${slug}.mdx`)
 
